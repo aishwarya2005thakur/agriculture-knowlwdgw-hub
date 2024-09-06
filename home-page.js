7f4b9ce2c60ce3d12cd8bc4d79de9232f11421da
aishwarya2005thakur <176339556+aishwarya2005thakur@users.noreply.github.com>
2024-09-06 12:17:42 +0530

    image-slider added

--- home-page.js
+++ home-page.js
@@ -8,26 +8,36 @@ function toggleVisibility(sectionId) {
     } else {
         content.style.display = "none";
         content.classList.add('hidden');
-    }
+    } }
 
-    let slideBtnleft= document.getElementById("slide-btn-left")
-    let slideBtnright= document.getElementById("slide-btn-right")
+    let slideBtnleft = document.getElementById("slide-btn-left")
+    let slideBtnright = document.getElementById("slide-btn-right")
     let imgItem = document.querySelectorAll(".image-item")
 
     console.log( imgItem. length-1)
 
     let startSlider = 0
-    let endSlider =  imgItem. length-1
+    let endSlider =  (imgItem.length-1) * 100
 
 
     slideBtnleft.addEventListener("click",()=>{
-        alert("left btn")
+        if(startSlider < 0){
+            startSlider = startSlider + 100;
+           }
+          imgItem.forEach(element =>{
+              element.style.transform = `translateX(${startSlider}%)`
+          })
     })
 
     slideBtnright.addEventListener("click",()=>{
-        alert("right btn")
-    })
+         if(startSlider >= -endSlider + 100){
+          startSlider = startSlider - 100;
+         }
+        imgItem.forEach(element =>{
+            element.style.transform = `translateX(${startSlider}%)`
+        })
+        
 
-    imgItem.style.transform = ` translateX(${startSlider})`}
+ } ) 
 
     
