01000bd026be372858b91a598c00ce6bc768f70e
aishwarya2005thakur <176339556+aishwarya2005thakur@users.noreply.github.com>
2024-09-06 06:30:16 +0530

    code modified=

--- home-page.js
+++ home-page.js
@@ -9,4 +9,25 @@ function toggleVisibility(sectionId) {
         content.style.display = "none";
         content.classList.add('hidden');
     }
-}
+
+    let slideBtnleft= document.getElementById("slide-btn-left")
+    let slideBtnright= document.getElementById("slide-btn-right")
+    let imgItem = document.querySelectorAll(".image-item")
+
+    console.log( imgItem. length-1)
+
+    let startSlider = 0
+    let endSlider =  imgItem. length-1
+
+
+    slideBtnleft.addEventListener("click",()=>{
+        alert("left btn")
+    })
+
+    slideBtnright.addEventListener("click",()=>{
+        alert("right btn")
+    })
+
+    imgItem.style.transform = ` translateX(${startSlider})`}
+
+    
