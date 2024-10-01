86d7a830282fc7f362e1ca11d7e9f93b88b5e7ce
aishwarya2005thakur <176339556+aishwarya2005thakur@users.noreply.github.com>
2024-10-01 10:39:14 +0530

    Update user-page.js

--- user-page.js
+++ user-page.js
@@ -1,7 +1,3 @@
-
-
-
-      
 function toggleVisibility(sectionId) {
     var content = document.getElementById(sectionId);
     if (content.style.display === "none" || content.classList.contains('hidden')) {
